cfe91807bc4b8b22aaa315eac67750e86fedb4f8
Hamzeen. H <hamzeen@gmail.com>
2018-12-02 13:52:58 +0100

    Update contacts_book.js

--- contacts_book.js
+++ contacts_book.js
@@ -1,8 +1,10 @@
 var uid = 1;
+
 function ContactController($scope) {
 
-    $scope.contacts = [
-        { 
+  $scope.newcontact = { gender: "MALE", graduate: "NO" };
+  $scope.contacts = [
+    {
           id: 0,
           name: "Ilham",
           stuId: "1999",
@@ -12,49 +14,55 @@ function ContactController($scope) {
           donation: "LKR 9,000",
           address: "60, Katugahwatte, Dewanagala, Sri Lanka",
           gender: "MALE"
-        }
-    ];
+    }
+  ];
 
-    $scope.jsonObj = {
-        name: "1990",
-        students: $scope.contacts
-    };
+  $scope.jsonObj = {
+    name: "1990",
+    students: $scope.contacts
+  };
 
-    $scope.saveContact = function() {
-        if($scope.newcontact.id == null) {
+  $scope.refreshForm = function() {
+    $scope.newcontact = { gender: "MALE", graduate: "NO" };
+  };
+
+  $scope.saveContact = function() {
+        if ($scope.newcontact.id == null) {
           //if this is new contact, add it in contacts array
           $scope.newcontact.id = uid++;
           $scope.contacts.push($scope.newcontact);
         } else {
           // update existing contact
           for(i in $scope.contacts) {
-            if($scope.contacts[i].id == $scope.newcontact.id) {
+            if ($scope.contacts[i].id == $scope.newcontact.id) {
               $scope.contacts[i] = $scope.newcontact;
             }
           }
         }
 
-        //clear the add contact form
-        $scope.newcontact = {};
-    };
+    // refresh form
+    $scope.refreshForm();
+  };
 
-    $scope.delete = function(id) {
-        //search contact with given id and delete it
-        for(i in $scope.contacts) {
-            if($scope.contacts[i].id == id) {
-                $scope.contacts.splice(i,1);
-                $scope.newcontact = {};
-            }
-        }
-    };
+  $scope.delete = function(id) {
+    // search contact with given id and delete it
+    for(i in $scope.contacts) {
+      if ($scope.contacts[i].id == id) {
 
-    $scope.edit = function(id) {
+        $scope.contacts.splice(i, 1);
+        // refresh form
+        $scope.refreshForm();
+      }
+    }
+  };
+
+  $scope.edit = function(id) {
     //search contact with given id and update it
-        for(i in $scope.contacts) {
-            if($scope.contacts[i].id == id) {
-                //we use angular.copy() method to create copy of original object
-                $scope.newcontact = angular.copy($scope.contacts[i]);
-            }
-        }
-    };
+    for(i in $scope.contacts) {
+      if ($scope.contacts[i].id == id) {
+        // we use angular.copy() method to create copy of original object
+        $scope.newcontact = angular.copy($scope.contacts[i]);
+      }
+    }
+  };
 }
